b2341d2219d640839a2a7e39988f2c864ac4fbdc
Iago <iagofalcom@hotmail.com>
2019-05-08 01:27:39 -0300

    Adicionando SharedPreferences para persistir os dados Adicionando o putFloat com chave e valor Utilizando o commit() para salvar os dados

--- app/src/main/java/br/com/unisys/MainActivity.java
+++ app/src/main/java/br/com/unisys/MainActivity.java
@@ -1,5 +1,8 @@
 package br.com.unisys;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
@@ -13,10 +16,13 @@ import br.com.unisys.util.SecurityPreferences;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Double valorAlcool;
-    private Double valorGasolina;
+    private Float valorAlcool;
+    private Float valorGasolina;
     private EditText Alcool;
     private EditText Gasolina;
+    public static final String MyPREFERENCES = "MyPrefs";
+
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +32,22 @@ public class MainActivity extends AppCompatActivity {
         Alcool = findViewById(R.id.EtanoEdt);
         Gasolina = findViewById(R.id.GasolinaEdt);
 
+        sharedPreferences = getSharedPreferences(MyPREFERENCES,Context.MODE_PRIVATE);
+
         Button calcular = (Button) findViewById(R.id.CalcularBtn);
         calcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                valorAlcool = Double.parseDouble(Alcool.getText().toString());
-                valorGasolina = Double.parseDouble(Gasolina.getText().toString());
+                valorAlcool = Float.parseFloat(Alcool.getText().toString());
+                valorGasolina = Float.parseFloat(Gasolina.getText().toString());
+
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                editor.putFloat("VALORALCOOL",valorAlcool);
+                editor.putFloat("VALORGASOLINA",valorGasolina);
+
+                editor.commit();
 
                 //valorAlcool = Double.parseDouble(findViewById(R.id.EtanoEdt).toString());
                 //valorGasolina = Double.parseDouble(findViewById(R.id.GasolinaEdt).toString());
